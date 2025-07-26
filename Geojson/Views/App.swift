82634c15c43d03953451b3f088862133fdec38e0
Jack Finnis <jack.finnis@icloud.com>
2025-07-26 09:52:09 +0100

    add todos

--- Geojson/Views/App.swift
+++ Geojson/Views/App.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 import SwiftData
 
+// Add more than one file
+// Query on the name attribute or others
+// Android version
+
 // https://cycling.data.tfl.gov.uk/CycleRoutes/CycleRoutes.json
 // https://www.google.com/maps/d/u/0/edit?mid=1SvfUi70Q0zSnkRsslNNGDfLixF39NmA
 // https://www.google.com/maps/d/u/0/kml?mid=1SvfUi70Q0zSnkRsslNNGDfLixF39NmA&forcekml=1
